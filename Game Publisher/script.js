bbcffe861e0aefad3669cb5d68706d1dbe18f7e7
Oscar-Gerdtsson <oscar.gerdtsson@elev.ga.lbs.se>
2026-02-27 10:49:52 +0100

    Update script.js

--- Game Publisher/script.js
+++ Game Publisher/script.js
@@ -1,18 +1,18 @@
 
-var set;//variable for a setinterval function that switches images in the image loopers
+var set;//variabel för en setinterval function som byter mellan bilderna i mina image loopers
 
-//this gets the image looper, and the label and linker if it can
+//detta hämtar image looper diven och label/länk om det finns
 let car =document.getElementsByClassName("imgcarousel");
 let ps =document.getElementsByClassName("namecarousel");
 let lnk= document.getElementById("gamescarousellink");
-if (car.length>0) {//if there is an image looper on the page, start the setinterval
-    car[0].firstElementChild.classList.add("displayimg");//the displayimg class is used as a marker to know what the current image is
-    car[0].firstElementChild.style.display = "block";//start displaying the first image and start the interval
+if (car.length>0) {//om det finns en image looper, starta setinterval
+    car[0].firstElementChild.classList.add("displayimg");//displayimg används för att markera vilken bild som visas
+    car[0].firstElementChild.style.display = "block";//börja displaya första bilden och starta intervallen
     var set = setInterval(() => changeImg(true), 3000);
-    if (ps.length >0) {//if there is a label, change it by getting the name of the file that contains the current image
+    if (ps.length >0) {//om det finns en label, ändra den till spelet som visas genom att ta namnet på filen där bilden sparas
         ps[0].innerHTML = car[0].firstElementChild.getAttribute("src").split("/").reverse()[1];
     }
-    if (lnk !== null) {// if there is a link, change it based on the label name
+    if (lnk !== null) {// om det finns en länk, ändra den till spelet baserat på labelen
         lnk.setAttribute("href", ps[0].innerHTML + "html.html");
     }
 }
@@ -20,15 +20,15 @@ if (car.length>0) {//if there is an image looper on the page, start the setinter
 
 
 
-function changeImg(next){//change image in an image looper, "next" is a bool for knowing whether to go to the next or the previous image
+function changeImg(next){//ändrar bilden i en image looper, "next" är en bool som är true om man ska byta till nästa image och false om man ska byta till den förra
     window.clearInterval(set);
-    set = setInterval(() => changeImg(true), 3000); //restart the interval
+    set = setInterval(() => changeImg(true), 3000); //starta om intervallen
 
-    let imgs = document.getElementsByClassName("imgcarousel")[0];//get the current image and hide it
+    let imgs = document.getElementsByClassName("imgcarousel")[0];//göm bilden som visas just nu
     let curimg = imgs.getElementsByClassName("displayimg")[0];
     curimg.classList.remove("displayimg");
     curimg.style.display = "none";
-    if (next) {//checks if it should change to the previous or next image, and if it cant switch to that direction anymore, loop to the other end of the list
+    if (next) {//kollar om den ska byta till nästa eller förra bilden, och om den inte kan byta åt det hållet längre, byt den till bilden på andra sidan av listn
         if (curimg == imgs.lastElementChild)
          {imgs.firstElementChild.classList.add("displayimg");}
          else
@@ -39,13 +39,14 @@ function changeImg(next){//change image in an image looper, "next" is a bool for
         else
         {curimg.previousElementSibling.classList.add("displayimg");}
     }
-    let newimg =imgs.getElementsByClassName("displayimg")[0];//get the new image and display it
+    let newimg =imgs.getElementsByClassName("displayimg")[0];//börja displaya nya bilden
     newimg.style.display = "block";
     let ps =document.getElementsByClassName("namecarousel");
-    if (ps.length >0) {//get the label and replace it by the same method as before
+    if (ps.length >0) {//byt label på samma sätt som innan
         ps[0].innerHTML = newimg.getAttribute("src").split("/").reverse()[1];
     }
-    if (lnk !== null) {//get the link and replace it by the same method as before
+    if (lnk !== null) {//byt länk på samma sätt som innan
         lnk.setAttribute("href", ps[0].innerHTML + "html.html");
     }
 }
+
